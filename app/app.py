dc6efb659dcf3473385694339f9d5107d92cf021
Vidyesh Churi <vidyesh95@gmail.com>
2025-12-17 17:41:00 +0530

    feat: Add `/blog/{id}` endpoint for fetching blogs by integer ID, rename `myBlogs` to `db`, and update blog keys to integers.

--- app/app.py
+++ app/app.py
@@ -3,27 +3,27 @@ from typing import Annotated
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-myBlogs = {
-    "1" : {
+db = {
+    1: {
         "title": "hello",
         "description": "world",
         "content": "Lorem ipsum The Alola region has no gyms. You can earn the Trio Badge at Striaton Gym. PokéManiac "
-                   "visited Goldenrod Department Store in Johto."
+        "visited Goldenrod Department Store in Johto.",
     },
-    "2" : {
+    2: {
         "title": "hello",
         "description": "world",
         "content": "Lorem ipsum Machop is a Superpower Pokémon. Commander spotted Kangaskhan in Unova. The leader at "
-                   "Shalour Gym specializes in Fighting Pokémon. You can find the Eterna Gym in Sinnoh. You can earn "
-                   "the Basic Badge at Nacrene Gym."
+        "Shalour Gym specializes in Fighting Pokémon. You can find the Eterna Gym in Sinnoh. You can earn "
+        "the Basic Badge at Nacrene Gym.",
     },
-    "3" : {
+    3: {
         "title": "hello",
         "description": "world",
         "content": "Lorem ipsum Socialite used a Net Ball to catch Victreebel. Tauros used Tail Whip. Voltorb is a "
-                   "Ball Pokémon. You can find the Dewford Gym in Hoenn. Haunter used Dream Eater. You can find the "
-                   "Fuchsia Gym in Kanto."
-    }
+        "Ball Pokémon. You can find the Dewford Gym in Hoenn. Haunter used Dream Eater. You can find the "
+        "Fuchsia Gym in Kanto.",
+    },
 }
 
 
@@ -41,13 +41,26 @@ class Blogs(BaseModel):
     blog: Blog
 
 
-def get_blog(blog: Annotated[Blog, "Read individual Blog"]) -> tuple[int, str, str]:
-    return blog.id, blog.title, blog.content
+def read_blog(id: int) -> Blog:
+    return Blog(
+        id=id,
+        title=db[id]["title"],
+        description=db[id]["description"],
+        content=db[id]["content"],
+    )
 
 
+@app.get("/blog/{id}")
+async def get_blog(id: int) -> Blog:
+    results = read_blog(id)
+    return results
+
+
+@app.get("/blogs")
 def get_blogs(id: int, title: str, description: str):
     return title, description
 
-print(get_blog(Blog(id=1, title="hello",description="world",content="Lorem ipsum The Alola region has no gyms. You can earn the Trio Badge at Striaton "
-                       "Gym. PokéManiac visited Goldenrod Department Store in Johto.")))
-print(get_blogs)
+
+# print(get_blog(Blog(id=1, title="hello",description="world",content="Lorem ipsum The Alola region has no gyms. You can earn the Trio Badge at Striaton "
+#                        "Gym. PokéManiac visited Goldenrod Department Store in Johto.")))
+# print(get_blogs)
