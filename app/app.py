016bdca3455aeffc6cc285d8fbe0de7f10111e0a
Vidyesh Churi <vidyesh95@gmail.com>
2025-12-17 09:37:48 +0530

    feat: add `myBlogs` dictionary with sample blog data

--- app/app.py
+++ app/app.py
@@ -1,3 +1,27 @@
+myBlogs = {
+    "1" : {
+        "title": "hello",
+        "description": "world",
+        "content": "Lorem ipsum The Alola region has no gyms. You can earn the Trio Badge at Striaton Gym. PokéManiac "
+                   "visited Goldenrod Department Store in Johto."
+    },
+    "2" : {
+        "title": "hello",
+        "description": "world",
+        "content": "Lorem ipsum Machop is a Superpower Pokémon. Commander spotted Kangaskhan in Unova. The leader at "
+                   "Shalour Gym specializes in Fighting Pokémon. You can find the Eterna Gym in Sinnoh. You can earn "
+                   "the Basic Badge at Nacrene Gym."
+    },
+    "3" : {
+        "title": "hello",
+        "description": "world",
+        "content": "Lorem ipsum Socialite used a Net Ball to catch Victreebel. Tauros used Tail Whip. Voltorb is a "
+                   "Ball Pokémon. You can find the Dewford Gym in Hoenn. Haunter used Dream Eater. You can find the "
+                   "Fuchsia Gym in Kanto."
+    }
+}
+
+
 def get_blogs(title: str, description: str):
     return title, description
 
