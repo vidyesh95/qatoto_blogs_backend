3db52ea853ff3dcb9dfaec81bda788bfdc3f0382
Vidyesh Churi <vidyesh95@gmail.com>
2025-12-17 09:25:46 +0530

    feat: Add `get_blogs` and `get_blog` functions with example usage.

---
+++ app/app.py
@@ -0,0 +1,9 @@
+def get_blogs(title: str, description: str):
+    return title, description
+
+def get_blog(title: str, content: str):
+    return title, content
+
+print(get_blogs("hello","world"))
+print(get_blog("hello","Lorem ipsum The Alola region has no gyms. You can earn the Trio Badge at Striaton "
+                       "Gym. PokéManiac visited Goldenrod Department Store in Johto."))
