f1c9b92fd7f548d76638d37de7144b98cdedc080
Vidyesh Churi <vidyesh95@gmail.com>
2025-12-23 15:31:31 +0530

    feat: Set up asynchronous database connection and session management.

--- app/db.py
+++ app/db.py
@@ -1,9 +1,41 @@
 """
 This file contains the database connection and functions
 """
-from sqlalchemy import create_engine
+from typing import AsyncGenerator
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/postgres"
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
 # DATABASE_URL = "sqlite+aiosqlite:///./test.db"  # noqa: ERA001
 
-async_engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+class Base(DeclarativeBase):
+    """
+    Base class for SQLAlchemy models
+
+    All models should inherit from this class.
+    """
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
+
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(async_engine, expire_on_commit=False)
+
+async def get_async_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
+
